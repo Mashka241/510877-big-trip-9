91824fea1eedf28457258531016e0722190b839e
Maria Ivanova <ivanova.m241@gmail.com>
2019-08-26 09:06:49 +0300

    add data

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,20 +1,17 @@
+import {
+  filters
+} from './../data/data';
+
+const createFilterTemplate = ({id, checked}) => {
+  return `<div class="trip-filters__filter">
+            <input id="filter-${id}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${id}" ${checked ? `checked` : ``}>
+            <label class="trip-filters__filter-label" for="filter-${id}">${id}</label>
+          </div>`;
+};
+
 export const createFiltersTemplate = () => {
   return `<form class="trip-filters" action="#" method="get">
-  <div class="trip-filters__filter">
-    <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-    <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-    <label class="trip-filters__filter-label" for="filter-future">Future</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-    <label class="trip-filters__filter-label" for="filter-past">Past</label>
-  </div>
-
-  <button class="visually-hidden" type="submit">Accept filter</button>
-</form>`;
+    ${filters.map(createFilterTemplate).join(``)}
+    <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`;
 };
